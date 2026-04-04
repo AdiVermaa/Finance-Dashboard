9f41b191743019e9288d797ab64afb1fefaee2ed
Aditya Verma <adityavermaa2006@gmail.com>
2026-04-04 23:13:12 +0530

    Fix axios baseURL for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api', timeout: 10000 });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '/api', timeout: 10000 });
 
 // Attach token to every request
 api.interceptors.request.use((config) => {
